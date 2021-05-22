3d3f62760ac007bfdf2e3d3588defd870e366de6
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-05-22 18:48:29 -0400

    second spreed

--- Day1/myFirst.js
+++ Day1/myFirst.js
@@ -1,3 +1,13 @@
-function filterEven(arr){
-    
-}
+let family = {
+    fname: "adu",
+    lname: "kasa",
+    address: {
+        zip: "Embamadre"
+    }
+};
+let copyA = { ...family };
+copyA.address = { ...copyA.address }
+copyA.fname = "Adonay";
+copyA.lname = "Kassa";
+family = { ...copyA }
+console.log(copyA)
